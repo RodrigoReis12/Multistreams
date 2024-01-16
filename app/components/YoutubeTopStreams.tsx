efbf73f19a57c82b30aa252488522cafd960d6a0
RodrigoReis12 <rodrigorebartini@hotmail.com>
2024-01-15 22:54:20 -0300

    fixed more more errors

--- app/components/YoutubeTopStreams.tsx
+++ app/components/YoutubeTopStreams.tsx
@@ -8,7 +8,7 @@ import type { RootState } from '../modules/store'
 
 export const YoutubeTopStreams = () => {
 
-  const [result, setResult] = useState({ message: { data: [{ user_login: "", thumbnail_url: "" }] } })
+  const [result, setResult] = useState({ message: { data: [{ user_login: "", thumbnail_url: "" }] , platform: ""}})
   const [isLoading, setIsLoading] = useState(true)
   const session = useSelector((state: RootState) => state.counter.youtubeSession)
 
@@ -34,7 +34,7 @@ export const YoutubeTopStreams = () => {
       ( isLoading ? <> <Skeleton className='h-12 w-36' /> </>
       :
       <div className='xl:pl-32 xl:pr-32 sm:pl-16 sm:pr-16 select-none sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6'>
-        {result.message.data.map((link) => (
+        {result.message.data.map((link: any) => (
           <div key={link.thumbnail_url} className='p-1'>
             <ThumbCard name={link.channel_name} viewCount={link.viewer_count} thumbUrl={link.thumbnail_url} title={link.channel_title} game={link.game_name} platform={result.message.platform} chnId={link.video_id} />
           </div>
